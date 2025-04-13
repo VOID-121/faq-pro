6b899cfdb5acdf605719c2053be4a9d21a14b066
Rakibul Islam <rakibulislamrifatt@gmail.com>
2025-04-13 17:15:49 +0600

    feat(page) little bug fixing deploy

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,8 +8,6 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import {
   Card,
-  CardContent,
-  CardHeader,
   CardTitle,
   CardDescription,
 } from "@/components/ui/card";
